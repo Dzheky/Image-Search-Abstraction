38791fc3aa891ef94551091c8ddd9297dedfd479
Evgeny Klimenchenko <zklimenchenko@gmail.com>
2016-07-16 14:12:49 +0000

    finished project

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var express = require("express");
+var Bing = require("node-bing-api")({accKey: process.env.API_KEY});
+var mongo = require("mongodb").MongoClient;
+var url = require("url");
+var dburl = 'mongodb://'+process.env.DB_LOGIN+':123456789@ds023245.mlab.com:23245/image-search'
 
 var app = express();
 
@@ -8,6 +12,65 @@ app.get('/', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     
     res.sendFile('index.html');
+})
+
+mongo.connect(dburl, function(dberr, db) {
+    if(dberr) throw dberr;
+    var collection = db.collection('latest');
+    //get latest search results
+    app.get('/latest/', function(req, res) {
+        res.writeHead(200, {'Content-Type':'application/JSON'});
+        collection.findOne({
+            _id: '1'
+        }, function(err, doc) {
+            if (err) throw err;
+            res.end(JSON.stringify(doc.data));
+        })
+    })
+    //get the search results
+    app.get('/:search*', function(req, res) {
+        res.writeHead(200, {'Conten-Type':'application/JSON'});
+        var searchResults;
+        var skip = 0;
+        var query = url.parse(req.url).query;
+        //if query exists deal with it
+        if(query) {
+            if(query.split('=')[0] == 'offset') {
+                skip = +query.split('=')[1].replace('&', '');
+            }
+        }
+        //search bing images
+        Bing.images(req.params.search, {skip: skip, top: 10}, function(err, response, body) {
+            if (err) throw err;
+            var latest;
+            collection.findOne({
+                _id: '1'
+            }, function(err, doc) {
+                if(err) throw err;
+                var date = new Date();
+                latest = doc;
+                latest.data.pop();
+                latest.data.unshift({
+                    term: req.params.search,
+                    when: date.toString()
+                });
+                collection.replaceOne({
+                    _id: '1'
+                }, latest);
+                
+            })
+            searchResults = body.d.results;
+            var filteredResults = [];
+            
+            searchResults.forEach(function(element) {
+                filteredResults.push({Title: element.Title, ImageURL: element.MediaUrl, SourceURL: element.SourceUrl})
+            })
+            
+            res.end(JSON.stringify(filteredResults));
+            
+            
+        })
+    })
 })
 
 var port = process.env.PORT || 8080;
